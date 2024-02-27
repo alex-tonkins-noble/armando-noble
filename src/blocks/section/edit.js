2365f4b9127bac4c92d2151288a2651af323fd47
Alex Tonkins <alex.tonkins@nobleperforms.co.uk>
2024-02-27 16:47:05 +0000

    add vertical content area to section block / update root editor structure styles

--- src/blocks/section/edit.js
+++ src/blocks/section/edit.js
@@ -1,13 +1,22 @@
 import "./editor.scss";
 import { __ } from "@wordpress/i18n";
-import { useBlockProps, useInnerBlocksProps } from "@wordpress/block-editor";
+import {
+	useBlockProps,
+	useInnerBlocksProps,
+	RichText,
+} from "@wordpress/block-editor";
 import { InspectorControls } from "@wordpress/block-editor";
-import { PanelBody, SelectControl } from "@wordpress/components";
+import { PanelBody, SelectControl, ToggleControl } from "@wordpress/components";
 import classNames from "classnames";
 
 export default function Edit(props) {
 	const { attributes, setAttributes } = props;
-	const { contentSize } = attributes;
+	const {
+		contentSize,
+		addSectionTitle,
+		sectionTitleContent,
+		sectionTitleHtmlTag,
+	} = attributes;
 
 	const containerClasses = classNames({
 		"wp-block-np-section__container": true,
@@ -31,15 +40,48 @@ export default function Edit(props) {
 		innerBlocksPropsExtras
 	);
 
-	const handleContentSizeChange = (val) => {
-		setAttributes({ contentSize: val });
-	};
-
 	const sizes = [
+		{ label: __("Small", "np-section"), value: "small" },
 		{ label: __("Regular", "np-section"), value: "regular" },
 		{ label: __("Wide", "np-section"), value: "wide" },
 		{ label: __("Full", "np-section"), value: "full" },
 	];
+
+	const availableHeadingTags = [
+		{ label: __("H1", "np-section"), value: "h1" },
+		{ label: __("H2", "np-section"), value: "h2" },
+		{ label: __("H3", "np-section"), value: "h3" },
+		{ label: __("H4", "np-section"), value: "h4" },
+		{ label: __("H5", "np-section"), value: "h5" },
+		{ label: __("H6", "np-section"), value: "h6" },
+		{ label: __("Paragraph", "np-section"), value: "p" },
+	];
+
+	// Define the section widths that are not available for use if the section title is enabled.
+	// Section content will clash with the section title if it is any wider than the regular content width.
+	const disallowedSectionTitleSizes = ["wide", "full"];
+
+	const handleContentSizeChange = (val) => {
+		setAttributes({ contentSize: val });
+
+		// Set addSectionTitle to false if the content size is above the regular content width.
+		if (disallowedSectionTitleSizes.includes(val)) {
+			setAttributes({ addSectionTitle: false });
+		}
+	};
+
+	const handleAddSectionTitleToggle = (val) => {
+		setAttributes({ addSectionTitle: val });
+
+		// If addSectionTitle is enabled and the current content width is not wide or full, then reset contentSize to "regular".
+		if (val && disallowedSectionTitleSizes.includes(contentSize)) {
+			setAttributes({ contentSize: "regular" });
+		}
+	};
+
+	const handleAddSectionTitleTagChange = (val) => {
+		setAttributes({ sectionTitleHtmlTag: val });
+	};
 
 	return (
 		<>
@@ -61,13 +103,48 @@ export default function Edit(props) {
 						}
 						onChange={handleContentSizeChange}
 					/>
+
+					<ToggleControl
+						label={__("Add Section Title", "np-section")}
+						checked={addSectionTitle}
+						onChange={handleAddSectionTitleToggle}
+					/>
+
+					{addSectionTitle && (
+						<SelectControl
+							label={__("Section Title HTML Tag", "np-section")}
+							help={__(
+								"Change the Tag / Heading Level of the Section Title here - beneficial for SEO.",
+								"np-section"
+							)}
+							value={sectionTitleHtmlTag}
+							options={
+								availableHeadingTags &&
+								availableHeadingTags.map((tag) => ({
+									label: tag.label,
+									value: tag.value,
+								}))
+							}
+							onChange={handleAddSectionTitleTagChange}
+						/>
+					)}
 				</PanelBody>
 			</InspectorControls>
 
 			<div {...innerBlocksProps}>
-				<div className="vertical-content">
-					<h3>Test Vertical Title</h3>
-				</div>
+				{addSectionTitle && (
+					<div className="vertical-content">
+						<RichText
+							tagName={sectionTitleHtmlTag}
+							value={sectionTitleContent}
+							onChange={(newTitle) =>
+								setAttributes({ sectionTitleContent: newTitle })
+							}
+							placeholder={__("Enter Section Title", "np-section")}
+							allowedFormats={[]}
+						/>
+					</div>
+				)}
 				<div className={containerClasses}>{children}</div>
 			</div>
 		</>
--- src/blocks/section/render.php
+++ src/blocks/section/render.php
@@ -1,21 +1,20 @@
 <?php
 
-/**
- * Generate classes if the Section Size and Content Size has been chosen AND they are not the default. This is to not have any unnecessary HTML Classes.
- * - Full = Section Default
- * - Regular = Content Default
- * NOTE: Also declared in edit.js
- */
-
 $contentSize = $attributes['contentSize'];
 $contentSizeClass = $contentSize ? ' content--size-'.$contentSize : '';
+
+$addSectionTitle = $attributes['addSectionTitle'];
+$sectionTitleContent = $attributes['sectionTitleContent'];
+$sectionTitleHtmlTag = $attributes['sectionTitleHtmlTag'] ? $attributes['sectionTitleHtmlTag'] : 'h2';
 
 ?>
 
 <div <?php echo get_block_wrapper_attributes(['class' => '']); ?>>
-    <div class="vertical-content">
-        <h3>Test Vertical Title</h3>
-    </div>
+    <?php if ($addSectionTitle && $sectionTitleContent && $sectionTitleHtmlTag): ?>
+        <div class="vertical-content">
+            <<?php echo $sectionTitleHtmlTag ?>><?php echo $sectionTitleContent ?></<?php echo $sectionTitleHtmlTag ?>>
+        </div>
+    <?php endif; ?>
     <div class="wp-block-np-section__container<?php echo $contentSizeClass ?>">
         <?php echo $content ?>
     </div>
